e9151e6566e9e43de699ec7aab1b6c21f39c1c04
Robert Gomez <ambitiousways2017@outlook.com>
2026-04-17 21:02:16 -0700

    Update Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,20 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCgsOr1gT0z93AsJYeY8WuihkXN8S-gyYM",
+  authDomain: "routetracker-92b42.firebaseapp.com",
+  projectId: "routetracker-92b42",
+  storageBucket: "routetracker-92b42.firebasestorage.app",
+  messagingSenderId: "435769829648",
+  appId: "1:435769829648:web:05353e4d5c9bea75625d95",
+  measurementId: "G-YB468B9MLT"
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export default app;
