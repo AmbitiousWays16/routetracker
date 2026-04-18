dc6fe4c1aa3ba604ebfcb665680de6f9908d7290
Robert Gomez <ambitiousways2017@outlook.com>
2026-04-17 21:38:54 -0700

    commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,17 +4,11 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgsOr1gT0z93AsJYeY8WuihkXN8S-gyYM",
-  authDomain: "routetracker-92b42.firebaseapp.com",
-  projectId: "routetracker-92b42",
-  storageBucket: "routetracker-92b42.firebasestorage.app",
-  messagingSenderId: "435769829648",
-  appId: "1:435769829648:web:05353e4d5c9bea75625d95",
-  measurementId: "G-YB468B9MLT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
-const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export default app;
